df2e0200b14782d91320004409ae59af29f079e9
Van-Romel Neto <van.neto@mastercard.com>
2023-02-01 16:40:19 -0300

    Constants comment has been updated

--- src/main/java/com/mastercard/dis/mids/reference/constants/Constants.java
+++ src/main/java/com/mastercard/dis/mids/reference/constants/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     public static final String MIDS_TENANT = "MIDS";
     //    Configuration Constant
     public static final String X_ENCRYPTED_HEADER = "X-Encrypted-Payload";
-    //    Example Constants
+    //    Example Input Constants
     public static final String COUNTRY_CODE = "US";
     public static final String LOCALE = "en-US";
     //    REGEX Constant
